bf9e212993459d95c4c1035437079a75de4a41a1
Dmitriy Lyalin <134696508+dslialin@users.noreply.github.com>
2025-01-28 22:10:36 +0300

    cleaning bot.py

--- hse_telegram_bot_hometask/bot.py
+++ hse_telegram_bot_hometask/bot.py
@@ -20,7 +20,6 @@ def home():
     return "Bot is running!"
 
 def run_flask():
-    # Получаем порт от Render (или используем 5000 по умолчанию)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
@@ -29,27 +28,5 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    # Flask-сервер запускается в отдельном потоке
     threading.Thread(target=run_flask).start()
-    # Основной поток — это работа Telegram-бота
     asyncio.run(main())
-
-
-# import asyncio
-# from aiogram import Bot, Dispatcher
-# from config import TOKEN
-# from handlers import router
-# from middlewares import LoggingMiddleware
-
-# bot = Bot(token=TOKEN)
-# dp = Dispatcher()
-
-# dp.message.middleware(LoggingMiddleware())
-# dp.include_router(router)
-
-# async def main():
-#     print("Бот запущен")
-#     await dp.start_polling(bot)
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
